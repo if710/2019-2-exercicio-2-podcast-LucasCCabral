1a6431cfe34c1250994ea7cab0d7ff3adfd92160
Lucas Cabral <lucascostacabra@gmail.com>
2019-09-12 23:39:52 -0300

    Stable, downloading and reading xml

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,31 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
+    val XML_FILE = "https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        val parser = Parser
+        var items : List<ItemFeed>
+        //parser.downloadXML(XML_FILE)
+
+        doAsync {
+            Log.d("DEV-LOG", "Async Started")
+            items = parser.parse(XML_FILE)
+            Log.d("DEV-LOG", "Async Ended")
+            uiThread {
+                    Log.d("DEV-LOG", "UI Thread Started")
+                    items.forEach {
+                        Log.d("DEV-LOG", it.toString())
+                    }
+                    Log.d("DEV-LOG", "UI Thread Ended")
+                }
+            }
+        }
 }
--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/Parser.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/Parser.kt
@@ -1,15 +1,22 @@
 package br.ufpe.cin.android.podcast
 
+import android.util.Log
+import org.jetbrains.anko.doAsync
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserException
 import org.xmlpull.v1.XmlPullParserFactory
 
 import java.io.IOException
 import java.io.StringReader
+import java.net.HttpURLConnection
+import java.net.URL
 import java.util.ArrayList
+import android.os.AsyncTask.execute
+import java.io.InputStream
 
-object Parser {
 
+object Parser  {
+    //https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml
     //Se quiser, teste primeiro com o parser simples para exibir lista de titulos - sem informacao de link
     @Throws(XmlPullParserException::class, IOException::class)
     fun parserSimples(rssFeed: String): List<String> {
@@ -47,31 +54,70 @@ object Parser {
         return items
     }
 
+    fun downloadXML(rssFeed: String){
+        val url = URL(rssFeed)
+        val factory = XmlPullParserFactory.newInstance()
+        val parser = factory.newPullParser()
+        doAsync {
+            Log.d("DEV-LOG", "Async Started")
+            val input = url.openStream()
+            Log.d("DEV-LOG", "XML downloaded")
+            parser.setInput(input.reader())
+            Log.d("DEV-LOG", "setInput()")
+            while (parser.next() != XmlPullParser.END_DOCUMENT) {
+                if (parser.eventType == XmlPullParser.START_TAG) {
+                    val tag = parser.name
+                    //delimitando que estamos apenas interessados em tags <item>
+                    if (tag == "item") {
+                        var tag = ""
+                        while (parser.next() != XmlPullParser.END_TAG) {
+                            if (parser.eventType == XmlPullParser.START_TAG) {
+                                val tagAberta = parser.name
+                                //pegando as tags <tag>
+                                if (tagAberta == "tag") {
+                                    tag = parser.nextText()
+                                    Log.d("DEV-LOG", tag)
+                                } else if(tagAberta == "link"){
+
+                                } else {
+                                    parser.next()
+                                }
+                                parser.nextTag()
+                            }
+                        }
+                    }
+                }
+            }
+            Log.d("DEV-LOG", "Async Ended")
+        }
+    }
+
     //Este metodo faz o parsing de RSS gerando objetos ItemFeed
     @Throws(XmlPullParserException::class, IOException::class)
-    fun parse(rssFeed: String): List<ItemFeed> {
+    fun parse(rssFeed: String):List<ItemFeed> {
         val factory = XmlPullParserFactory.newInstance()
         val xpp = factory.newPullParser()
-        xpp.setInput(StringReader(rssFeed))
-        xpp.nextTag()
+        val url = URL(rssFeed)
+        val input = url.openStream()
+        xpp.setInput(input.reader())
+
         return readRss(xpp)
     }
 
     @Throws(XmlPullParserException::class, IOException::class)
     fun readRss(parser: XmlPullParser): List<ItemFeed> {
         val items = ArrayList<ItemFeed>()
-        parser.require(XmlPullParser.START_TAG, null, "rss")
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.eventType != XmlPullParser.START_TAG) {
-                continue
-            }
-            val name = parser.name
-            if (name == "channel") {
-                items.addAll(readChannel(parser))
-            } else {
-                skip(parser)
+        while (parser.next() != XmlPullParser.END_DOCUMENT) {
+            if (parser.eventType == XmlPullParser.START_TAG) {
+                val tag = parser.name
+                //delimitando que estamos apenas interessados em tags <item>
+                if (tag == "item") {
+                    var item = readItem(parser)
+                    items.add(item)
+                }
             }
         }
+
         return items
     }
 
@@ -107,12 +153,16 @@ object Parser {
             val name = parser.name
             if (name == "title") {
                 title = readData(parser, "title")
+                //Log.d("DEV-LOG", title)
             } else if (name == "link") {
                 link = readData(parser, "link")
+                //Log.d("DEV-LOG", link)
             } else if (name == "pubDate") {
                 pubDate = readData(parser, "pubDate")
+                //Log.d("DEV-LOG", pubDate)
             } else if (name == "description") {
                 description = readData(parser, "description")
+                //Log.d("DEV-LOG", description)
             } else {
                 skip(parser)
             }
